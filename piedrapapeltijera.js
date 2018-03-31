d50b987521c34726977a5ab7cb59068f8d121752
Agustín Ramiro Quetto Garay Lima <agustinquetto@gmail.com>
2018-03-31 16:17:26 -0300

    jugar2

--- piedrapapeltijera.js
+++ piedrapapeltijera.js
@@ -1,5 +1,5 @@
 function jugar () {
-  console.log('jugando')
+  console.log('jugar')
   let jugador1 = (Math.floor(Math.random() * (0 + 3) + 0)) // prompt('Ingrese piedra papel o tijera')
   let jugador2 = (Math.floor(Math.random() * (0 + 3) + 0))
   let elecciones = ['piedra', 'papel', 'tijera']
@@ -42,4 +42,39 @@ function jugar () {
   }
 }
 
+function jugar2 () {
+  console.log('jugar2')
+  let jugador1 = (Math.floor(Math.random() * (0 + 3) + 0)) // prompt('Ingrese piedra papel o tijera')
+  let jugador2 = (Math.floor(Math.random() * (0 + 3) + 0))
+
+  console.log(`Jugador1 eligio: ${jugador1}`)
+  console.log(`Jugador2 eligio: ${jugador2}`)
+
+  if ((jugador1 === 0 && jugador2 === 1) || (jugador2 === 0 && jugador1 === 1)) {
+    if (jugador1 === 1) {
+      console.log('Jugador1 (papel) gana')
+    }
+    if (jugador2 === 1) {
+      console.log('Jugador2 (papel) gana')
+    }
+  } else if ((jugador1 === 1 && jugador2 === 2) || (jugador2 === 1 && jugador1 === 2)) {
+    if (jugador1 === 2) {
+      console.log('Jugador1 (tijera) gana')
+    }
+    if (jugador2 === 2) {
+      console.log('Jugador2 (tijera) gana')
+    }
+  } else if ((jugador1 === 2 && jugador2 === 0) || (jugador2 === 2 && jugador1 === 0)) {
+    if (jugador1 === 0) {
+      console.log('Jugador1 (piedra) gana')
+    }
+    if (jugador2 === 0) {
+      console.log('Jugador2 (piedra) gana')
+    }
+  } else {
+    console.log('empate')
+  }
+}
+
 jugar()
+jugar2()
